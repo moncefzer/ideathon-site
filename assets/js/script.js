632877d8ca5c8631f529ba842a673b6b370f4f0a
moncefzer <monsefzermache0664@gmail.com>
2022-02-27 20:15:14 +0100

    add the concepts of ideathon

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,11 @@ let nav = document.querySelector(".navbar");
 toggle_btn.addEventListener("click", () => {
   nav.classList.toggle("open");
 });
+
+let navlinks = document.querySelectorAll(".navlink");
+
+for (let i = 0; i < navlinks.length; i++) {
+  navlinks[i].addEventListener("click", () => {
+    nav.classList.toggle("open");
+  });
+}
